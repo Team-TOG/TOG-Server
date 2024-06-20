4b4460306e92ef1248454a190081b0afc242a0c0
robo <robolab1902@naver.com>
2024-06-21 08:12:14 +0900

    웹에서 채팅하기 업로드. DB 연결과 스트림 처리 필요

--- Chat/views.py
+++ Chat/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.conf import settings
+from django.http import JsonResponse
+import openai
+import os
+
+openai.api_key = getattr(settings, 'OPEN_API_KEY', None)
 
 # Create your views here.
-def home(request):
-    return HttpResponse("hello world!")
+def get_completion(prompt): 
+    print(prompt) 
+    query = openai.ChatCompletion.create( 
+        model="gpt-4-turbo",
+        messages=[
+            {'role':'user','content': prompt}
+        ], 
+        max_tokens=4096, 
+        n=1, 
+        stop=None, 
+        temperature=0, 
+    ) 
+    response = query.choices[0].message["content"]
+    print(response) 
+    return response 
+
+def prompt(request):
+    if request.method == 'POST': 
+        prompt = request.POST.get('prompt') 
+        prompt=str(prompt)
+        response = get_completion(prompt)
+        return JsonResponse({'response': response}) 
+    return render(request, 'index.html') 
