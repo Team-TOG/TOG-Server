ecb8fab2892bbbe2d3c54f30bcb92d00a4df7218
robo <robolab1902@naver.com>
2024-06-23 14:23:23 +0900

    Streaming Http Response 로 올리고 있는데 자꾸 웹에선 잘 받질 못한다.

--- Chat/views.py
+++ Chat/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.conf import settings
-from django.http import JsonResponse
+from django.http import StreamingHttpResponse
 import openai
 import os
+import time
+import json
 
 openai.api_key = getattr(settings, 'OPEN_API_KEY', None)
 
 # Create your views here.
 def get_completion(prompt): 
     print(prompt) 
+
+    start_time = time.time()
+
     query = openai.ChatCompletion.create( 
         model="gpt-4-turbo",
         messages=[
@@ -17,10 +22,19 @@ def get_completion(prompt):
         max_tokens=4096, 
         n=1, 
         stop=None, 
-        temperature=0, 
+        temperature=0,
+        stream=True
     ) 
-    response = query.choices[0].message["content"]
-    print(response) 
+
+    for chunk in query:
+        chatcompletion_delta = chunk["choices"][0].get("delta", {})
+        data = json.dumps(dict(chatcompletion_delta))
+        yield f'data: {data}\n\n'
+
+    response = StreamingHttpResponse(event_stream(), content_type="text/event-stream")
+    response['X-Accel-Buffering'] = 'no'  # Disable buffering in nginx
+    response['Cache-Control'] = 'no-cache'  # Ensure clients don't cache the data
+    
     return response 
 
 def prompt(request):
@@ -28,5 +42,7 @@ def prompt(request):
         prompt = request.POST.get('prompt') 
         prompt=str(prompt)
         response = get_completion(prompt)
-        return JsonResponse({'response': response}) 
+        #return JsonResponse({'response': response})
+        return response
+
     return render(request, 'index.html') 
